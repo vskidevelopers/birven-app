134d247be4793ea8590030584feb0a53c13df2e3
Ben <v.skidevelopers@gmail.com>
2024-04-26 23:19:37 +0300

    const user = auth.currentUser;

--- src/auth/PrivateRoutes.jsx
+++ src/auth/PrivateRoutes.jsx
@@ -8,7 +8,7 @@ export default function PrivateRoutes() {
   const [authUser, setAuthUser] = useState();
   const [loginState, setLoginState] = useState(false);
   //   const { user } = useAuth();
-  const user = true;
+  const user = auth.currentUser;
   const navigate = useNavigate();
   const location = useLocation();
 
