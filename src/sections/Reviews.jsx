46fa55483549fd05f9da65d84aede1dafd4dfa22
Ben <v.skidevelopers@gmail.com>
2024-05-06 12:11:24 +0300

    fixed  revews display

--- src/sections/Reviews.jsx
+++ src/sections/Reviews.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { StarIcon } from "@heroicons/react/24/outline";
 
@@ -6,8 +6,22 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 import { Autoplay, Navigation } from "swiper/modules";
+import { useReviewsFunctions } from "@/firebase/firbase";
 
 export default function Reviews() {
+  const { getAllReviewsbyStatus } = useReviewsFunctions();
+  const [reviews, setReviews] = useState([]);
+  const reviewStatus = "approved";
+  const getApprovedReviews = async () => {
+    const approvedReviews = await getAllReviewsbyStatus(reviewStatus);
+    console.log("approvedReviews >> ", approvedReviews);
+    setReviews(approvedReviews?.data);
+  };
+
+  useEffect(() => {
+    getApprovedReviews();
+  }, []);
+
   return (
     <>
       <h2 className="text-center text-3xl font-semibold py-8">
@@ -28,88 +42,34 @@ export default function Reviews() {
           modules={[Autoplay, Navigation]}
           className="mySwiper h-full w-5/6"
         >
-          <SwiperSlide>
-            <div className="py-10 md:px-28 ">
-              <div className="flex justify-center py-3">
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-              </div>
-              <div className="text-center my-3">
-                <p className="font-serif text-xl md:text-2xl">
-                  “Top-notch customer experience! From browsing the website to
-                  delivery, everything was smooth and efficient. The equipment
-                  arrived in perfect condition, and I'm thrilled with my
-                  purchase.”
-                </p>
-              </div>
+          {reviews.map((review, index) => (
+            <SwiperSlide key={index}>
+              <div className="py-10 md:px-28 ">
+                <div className="flex justify-center py-3">
+                  {[...Array(review.rating)].map((_, i) => (
+                    <StarIcon key={i} className="h-4 w-4 text-emerald-900" />
+                  ))}
+                </div>
+                <div className="text-center my-3">
+                  <p className="font-serif text-xl md:text-2xl">
+                    "{review.review}"
+                  </p>
+                </div>
 
-              <div className="text-center">
-                <p className="text-sm mt-5 font-medium text-teal-600 uppercase">
-                  Ben Muchemi, Ke
-                </p>
+                <div className="text-center">
+                  <p className="text-sm mt-5 font-medium text-teal-600 uppercase">
+                    {review.name}, KE{" "}
+                  </p>
+                </div>
               </div>
-            </div>
-          </SwiperSlide>
-
-          {/* swiper */}
-          <SwiperSlide>
-            <div className="py-10 md:px-28">
-              <div className="flex justify-center py-3">
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-              </div>
-              <div className="text-center my-3">
-                <p className="font-serif text-xl md:text-2xl ">
-                  “Great variety and excellent service! I found everything I
-                  needed for my home gym setup at this store. The staff were
-                  knowledgeable and helped me make the right choices”
-                </p>
-              </div>
-
-              <div className="text-center">
-                <p className="text-sm mt-5 font-medium text-teal-600 uppercase">
-                  {" "}
-                  Maurice Mwangi, Ke
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="py-10 md:px-28">
-              <div className="flex justify-center py-3">
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-                <StarIcon className="h-4 w-4 text-emerald-900" />
-              </div>
-              <div className="text-center my-3">
-                <p className="font-serif text-xl md:text-2xl">
-                  “Exceptional quality equipment! I purchased a treadmill from
-                  this store, and I'm blown away by its durability and
-                  performance. Highly recommended!”!
-                </p>
-              </div>
-
-              <div className="text-center">
-                <p className="text-sm mt-5 font-medium text-teal-600 uppercase">
-                  Jane Doe, Ke
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
+            </SwiperSlide>
+          ))}
         </Swiper>
         <div className="absolute top-1/2 transform -translate-y-1/2 left-0 z-10">
-          <button className="swiper-button-prev"></button>
+          <button className="swiper-button-prev">&#8592;</button>
         </div>
         <div className="absolute top-1/2 transform -translate-y-1/2 right-0 z-10">
-          <button className="swiper-button-next"></button>
+          <button className="swiper-button-next">&#8594;</button>
         </div>
       </div>
     </>
